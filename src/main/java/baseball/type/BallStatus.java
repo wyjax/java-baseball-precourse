7f561e4fa5b2e1666998e5bfa6d7a81175bd19fb
wyjax <wyjax.dev@gmail.com>
2022-04-17 23:39:52 +0900

    feat: BallsStatus 개발

---
+++ src/main/java/baseball/type/BallStatus.java
@@ -0,0 +1,19 @@
+package baseball.type;
+
+public enum BallStatus {
+    STRIKE,
+    BALL,
+    NOTHING;
+
+    public static boolean isStrike(BallStatus status) {
+        return STRIKE.equals(status);
+    }
+
+    public static boolean isBall(BallStatus status) {
+        return STRIKE.equals(status);
+    }
+
+    public static boolean isNothing(BallStatus status) {
+        return STRIKE.equals(status);
+    }
+}
